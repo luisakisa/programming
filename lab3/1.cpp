edf15c33561501701a2e03a59343a9b52f550cba
luisakisa <72152407+luisakisa@users.noreply.github.com>
2021-04-09 10:31:46 +0700

    lab3

---
+++ lab3/1.cpp
@@ -0,0 +1,175 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+struct rabbit
+{
+	string kind;
+	double weight;
+	float height;
+	int age;
+};
+struct stack
+{
+	struct node
+	{
+		rabbit data;
+		node *prev;
+	};
+	node *top = NULL;
+	int count = 0;
+	bool push(rabbit);
+	bool pop(rabbit&);
+	void info();
+};
+bool stack::push(rabbit dt)
+{
+	if (!top)
+	{
+		top = new node;
+		top->prev = NULL;
+		count = 1;
+	}
+	else
+	{
+		node *temp;
+		temp = new node;
+		temp->prev = top;
+		top = temp;
+		count++;
+	}
+	top->data = dt;
+	return true;
+}
+bool stack::pop(rabbit& dt)
+{
+	if (!top) return false;
+	node *temp = top->prev;
+	dt = top->data;
+	delete top;
+	top = temp;
+	count--;
+	return true;
+}
+void stack::info()
+{
+	if (!top) cout << "stack is empty" << endl;
+	else
+	{
+		cout << endl << "stack info: " << endl;
+		cout << "\tstack size = " << count << endl;
+		cout << "\ttop kind = " << top->data.kind << endl;
+		cout << "\ttop weight = " << top->data.weight << endl;
+		cout << "\ttop height = " << top->data.height << endl;
+		cout << "\ttop age = " << top->data.age << endl << endl;
+	}
+}
+void print(stack &S, stack &V)
+{
+	rabbit dt;
+	while (S.count)
+	{
+		S.pop(dt);
+		cout << "\tkind = " << dt.kind << endl;
+		cout << "\tweight = " << dt.weight << endl;
+		cout << "\theight = " << dt.height << endl;
+		cout << "\tage = " << dt.age << endl << endl;
+		V.push(dt);
+	}
+	while (V.count)
+	{
+		V.pop(dt);
+		S.push(dt);
+	}
+}
+int main()
+{
+	stack S;
+	stack V;
+	rabbit dt;
+	ifstream file("rab.txt");
+	string line;
+	while (getline(file, line))
+	{
+		istringstream line_F(line);
+		line_F >> dt.kind >> dt.weight >> dt.height >> dt.age;
+		S.push(dt);
+	}
+	file.close();
+	int m = 1;
+	while (m)
+	{
+		S.info();
+		cout << "1. add to basket" << endl;
+		cout << "2. pull from basket" << endl;
+		cout << "3. clear basket" << endl;
+		cout << "4. show basket contents" << endl;
+		cout << "0. exit" << endl;
+		cin >> m;
+		switch (m)
+		{
+		case 1:
+		{
+			system("cls");
+			cout << "enter product specifications:" << endl;
+			cout << "kind = "; cin >> dt.kind;
+			cout << "weight = "; cin >> dt.weight;
+			cout << "height = "; cin >> dt.height;
+			cout << "age = "; cin >> dt.age;
+			S.push(dt);
+			system("cls");
+			break;
+		}
+		case 2:
+		{
+			system("cls");
+			rabbit dt_x;
+			bool metka = false;
+			cout << "enter product specifications:" << endl;
+			cout << "kind = "; cin >> dt_x.kind;
+			cout << "weight = "; cin >> dt_x.weight;
+			cout << "height = "; cin >> dt_x.height;
+			cout << "age = "; cin >> dt_x.age;
+			while (S.count)
+			{
+				S.pop(dt);
+				if (dt.kind != dt_x.kind || dt.weight != dt_x.weight || dt.height != dt_x.height || dt.age != dt_x.age)
+				{
+					V.push(dt);
+				}
+				else
+				{
+					metka = true;
+					break;
+				}
+			}
+			if (!metka) cout << "product not found" << endl;
+			while (V.count)
+			{
+				V.pop(dt);
+				S.push(dt);
+			}
+			system("pause");
+			system("cls");
+			break;
+		}
+		case 3:
+		{
+			system("cls");
+			while (S.count)
+			{
+				S.pop(dt);
+			}
+			break;
+		}
+		case 4:
+		{
+			system("cls");
+			print(S, V);
+			system("pause");
+			system("cls");
+			break;
+		}
+		}
+	}
+}
---
+++ lab3/2.cpp
@@ -0,0 +1,186 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+struct product
+{
+	int quantity;
+	double price;
+};
+struct stock
+{
+	struct node
+	{
+		product data;
+		node* next;
+	};
+	node* first = NULL;
+	int count = 0;
+	double full_cost = 0;
+	double income = 0;
+	bool push(product);
+	bool pop(product&);
+	void info();
+};
+bool stock::push(product dt)
+{
+	if (!first)
+	{
+		first = new node;
+		first->next = NULL;
+		first->data = dt;
+		count = 1;
+		full_cost += dt.price * dt.quantity;
+	}
+	else
+	{
+		node *temp;
+		temp = first;
+		while (temp->next != NULL) temp = temp->next;
+		temp->next = new node;
+		temp->next->data = dt;
+		temp->next->next = NULL;
+		count++;
+		full_cost += dt.price * dt.quantity;
+	}
+	return true;
+}
+bool stock::pop(product& dt)
+{
+	if (!first) return false;
+	node* temp = first->next;
+	dt = first->data;
+	delete first;
+	first = temp;
+	count--;
+	return true;
+}
+void stock::info()
+{
+	if (!first) cout << "The warehouse is empty" << endl;
+	else
+	{
+		cout << endl << "Info: " << endl;
+		cout << "\tNumber of products group = " << count << endl;
+		cout << "\tFirst quantity = " << first->data.quantity << endl;
+		cout << "\tFirst price = " << first->data.price << endl << endl;
+	}
+}
+bool check(stock Q, product dt_x)
+{
+	product dt;
+	stock::node *a;
+	a = Q.first;
+	int k = 0;
+	while (a->data.price <= dt_x.price & k != Q.count)
+	{
+		dt.price = a->data.price;
+		dt.quantity = a->data.quantity;
+		if (dt_x.quantity <= dt.quantity)
+		{
+			dt_x.quantity = 0;
+		}
+		else
+		{
+			dt_x.quantity -= dt.quantity;
+		}
+		if (dt_x.quantity == 0)
+		{
+			return true;
+		}
+		else
+		{
+			if (k < Q.count)
+			{
+				k++;
+				a = a->next;
+			}
+			else return false;
+		}
+	}
+	return false;
+}
+int main()
+{
+	stock Q;
+
+	product dt_x;
+	product dt, dt1 = { 5,20.5 }, dt2 = { 10,15.5 };
+
+
+	Q.push(dt1);
+	Q.push(dt2);
+	int m = 1;
+	while (m)
+	{
+		Q.info();
+		cout << "1. receipt" << endl;
+		cout << "2. sell" << endl;
+		cout << "3. report" << endl;
+		cout << "0. exit" << endl;
+		cin >> m;
+		switch (m)
+		{
+		case 1:
+		{
+			system("cls");
+			cout << "Enter the price and quantity of the product:" << endl;
+			cout << "Quantity = "; cin >> dt.quantity;
+			cout << "Price = "; cin >> dt.price;
+			Q.push(dt);
+			system("cls");
+			break;
+		}
+		case 2:
+		{
+			system("cls");
+			cout << "Enter the price and quantity of the product:" << endl;
+			cout << "Quantity = "; cin >> dt_x.quantity;
+			cout << "Price = "; cin >> dt_x.price;
+			dt = Q.first->data;
+			if (check(Q, dt_x))
+			{
+				while (dt_x.quantity)
+				{
+					if (dt_x.quantity <= dt.quantity)
+					{
+						Q.income += dt_x.quantity * (dt_x.price - dt.price);
+						Q.full_cost -= dt.price * dt_x.quantity;
+						dt.quantity -= dt_x.quantity;
+						dt_x.quantity = 0;
+						Q.first->data.quantity = dt.quantity;
+					}
+					else
+					{
+						Q.income += dt.quantity * (dt_x.price - dt.price);
+						Q.full_cost -= dt.price * dt.quantity;
+						dt_x.quantity -= dt.quantity;
+						dt.quantity = 0;
+					}
+					if (dt.quantity == 0)
+					{
+						Q.pop(dt);
+						dt = Q.first->data;
+					}
+				}
+				cout << "Completed" << endl;
+			}
+			else cout << "Сannot be done" << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+		case 3:
+		{
+			system("cls");
+			cout << "Count = " << Q.count << endl;
+			cout << "Full cost = " << Q.full_cost << endl;
+			cout << "Income = " << Q.income << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+		}
+	}
+}
+
