51f8b61d63bc63e98b12a72c178ed6f629032498
luisakisa <72152407+luisakisa@users.noreply.github.com>
2021-09-17 13:01:15 +0700

    lab1

---
+++ 1 курс/lab3/2.cpp
@@ -0,0 +1,202 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+struct product_group
+{
+	int quantity;
+	double price;
+};
+struct MyQueue
+{
+	struct Node
+	{
+		product_group data;
+		Node* next;
+	};
+	Node* First = NULL;
+	int count = 0;
+	double full_cost = 0;
+	double full_quantity = 0;
+	double income = 0;
+	bool push(product_group);
+	bool pop(product_group&);
+	void Delete(product_group&);
+	void Info();
+};
+bool MyQueue::push(product_group dt)
+{
+	if (!First)
+	{
+		First = new Node;
+		First->next = NULL;
+		First->data = dt;
+		count = 1;
+		full_cost += dt.price * dt.quantity;
+		full_quantity += dt.quantity;
+	}
+	else
+	{
+		Node* temp;
+		temp = First;
+		while (temp->next != NULL) temp = temp->next;
+		temp->next = new Node;
+		temp->next->data = dt;
+		temp->next->next = NULL;
+		count++;
+		full_cost += dt.price * dt.quantity;
+		full_quantity += dt.quantity;
+	}
+	return true;
+}
+bool MyQueue::pop(product_group& dt)
+{
+	if (!First) return false;
+	Node* temp = First->next;
+	dt = First->data;
+	delete First;
+	First = temp;
+	count--;
+	return true;
+}
+void MyQueue::Info()
+{
+	if (!First) cout << "warehouse is empty" << endl;
+	else
+	{
+		cout << endl << "Info " << endl;
+		cout << "\tNumbers of products group = " << count << endl;
+		cout << "\tFirst quantity = " << First->data.quantity << endl;
+		cout << "\tFirst price = " << First->data.price <<  endl;
+		cout << "\tNumbers of products = " << full_quantity << endl;
+	}
+}
+bool check(MyQueue Q, product_group dt_x)
+{
+	product_group dt;
+	MyQueue::Node* a;
+	a = Q.First;
+	int k = 0;
+	while (a->data.price <= dt_x.price & k != Q.count & dt_x.quantity <= Q.full_quantity)
+	{
+		dt.price = a->data.price;
+		dt.quantity = a->data.quantity;
+		if (dt_x.quantity <= dt.quantity)
+		{
+			dt_x.quantity = 0;
+		}
+		else
+		{
+			dt_x.quantity -= dt.quantity;
+		}
+		if (dt_x.quantity == 0)
+		{
+			return true;
+		}
+		else
+		{
+			if (k < Q.count)
+			{
+				k++;
+				a = a->next;
+			}
+			else return false;
+		}
+	}
+	return false;
+}
+int main()
+{
+	MyQueue Q;
+	product_group dt;
+	product_group dt_x;
+	ifstream file("prod.txt");
+	string line;
+	while (getline(file, line))
+	{
+		istringstream line_F(line);
+		line_F >> dt.quantity >> dt.price;
+		Q.push(dt);
+	}
+	file.close();
+	int m = 1;
+	while (m)
+	{
+		Q.Info();
+		cout << "1. Receipt" << endl;
+		cout << "2. Sell" << endl;
+		cout << "3. Report" << endl;
+		cout << "0. Exit" << endl;
+		cin >> m;
+		switch (m)
+		{
+		case 1:
+		{
+			system("cls");
+			cout << "Enter the price and quantity of the product:" << endl;
+			cout << "Quantity = "; cin >> dt.quantity;
+			cout << "Price = "; cin >> dt.price;
+			Q.push(dt);
+			system("cls");
+			break;
+		}
+		case 2:
+		{
+			system("cls");
+			cout << "Enter the price and quantity of the product:" << endl;
+			cout << "Quantity = "; cin >> dt_x.quantity;
+			cout << "Price = "; cin >> dt_x.price;
+			dt = Q.First->data;
+			if (check(Q, dt_x))
+			{
+				while (dt_x.quantity)
+				{
+					if (dt_x.quantity <= dt.quantity)
+					{
+						Q.income += dt_x.quantity * (dt_x.price - dt.price);
+						Q.full_cost -= dt.price * dt_x.quantity;
+						dt.quantity -= dt_x.quantity;
+						dt_x.quantity = 0;
+						Q.First->data.quantity = dt.quantity;
+					}
+					else
+					{
+						Q.income += dt.quantity * (dt_x.price - dt.price);
+						Q.full_cost -= dt.price * dt.quantity;
+						dt_x.quantity -= dt.quantity;
+						dt.quantity = 0;
+					}
+					if (dt.quantity == 0)
+					{
+						Q.pop(dt);
+						dt = Q.First->data;
+					}
+				}
+				cout << "Completed" << endl;
+			}
+			else cout << "Was not made :(" << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+		case 3:
+		{
+			system("cls");
+			cout << "Number of product = " << Q.count << endl;
+			cout << "Product price = " << Q.full_cost << endl;
+			cout << "Income from sale = " << Q.income << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+		}
+	}
+	Q.Delete(dt);
+	return 0;
+
+}
+void MyQueue::Delete(product_group& G)
+{
+	while (count != 0)
+		pop(G);
+}
---
+++ 1 курс/lab6/lab6.cpp
@@ -0,0 +1,186 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+struct Pie
+{
+  string filling;
+  float weight;
+  string restaurant;
+  float price;
+};
+Pie* Init(Pie);
+void Del(Pie*);
+void A_Print(Pie*);
+Pie* Init(Pie A)
+{
+  Pie* B = new Pie;
+  *B = A;
+  return B;
+}
+void Del(Pie* A)
+{
+  delete A;
+}
+void A_Print(Pie* A)
+{
+  cout << endl << endl;
+  cout << "Filling: " << A->filling << endl;
+  cout << "Weight: " << A->weight << endl;
+  cout << "Restaurant: " << A->restaurant << endl;
+  cout << "Price: " << A->price << endl;
+  cout << endl << endl;
+}
+struct Tree
+{
+  Pie* data;
+  Tree* left;
+  Tree* right;
+};
+bool sort_str(string a, string b)
+{
+  vector <string> v = {a, b};
+  sort(v.begin(), v.end());
+  if(v[0] == a) return true;
+  else return false;
+}
+void Add_price(Tree*& T, Pie* data)
+{
+  if(!T)
+  {
+    T = new Tree;
+    T->data = data;
+    T->left = NULL;
+    T->right = NULL;
+    return;
+  }
+  if(data->price < T->data->price) Add_price(T->left, data);
+  else Add_price(T->right, data);
+}
+void Add_color(Tree*& T, Pie* data)
+{
+  if(!T)
+  {
+    T = new Tree;
+    T->data = data;
+    T->left = NULL;
+    T->right = NULL;
+    return;
+  }
+  if(sort_str(data->filling, T->data->filling)) Add_color(T->left, data);
+  else Add_color(T->right, data);
+}
+void Print(Tree* T)
+{
+  if(!T) return;
+  Print(T->left);
+  A_Print(T->data);
+  Print(T->right);
+}
+void Delete(Tree*& T)
+{
+  if(T)
+  {
+    if(T->left) Delete (T->left);
+    if(T->right) Delete(T->right);
+    Del(T->data);
+    delete T;
+    T = NULL;
+  }
+}
+void Print_price(Tree* T, float price)
+{
+  if(!T) return;
+  Print_price(T->left, price);
+  if(T->data->price < price) A_Print(T->data);
+  Print_price(T->right, price);
+}
+void Print_color(Tree* T, char a)
+{
+  if(!T) return;
+  Print_color(T->left, a);
+  if(T->data->filling[0] == a) A_Print(T->data);
+  Print_color(T->right, a);
+}
+int main()
+{
+    Pie A;
+    Tree* T = NULL;
+    int m = 1;
+    while (m)
+    {
+        ifstream file("Flowers.txt");
+        string line;
+        system("cls");
+        cout << endl << endl;
+        cout << "1. Sort by filling" << endl;
+        cout << "2. Sort by price" << endl;
+        cout << "3. Show pies at a price below the specified price" << endl;
+        cout << "4. Show pies whose filling starts with your letter" << endl;
+        cout << "0. Exit" << endl << endl;
+        cin >> m;
+        switch (m)
+        {
+        case 1:
+        {
+            while (getline(file, line))
+            {
+                istringstream line_F(line);
+                line_F >> A.filling >> A.weight >> A.restaurant >> A.price;
+                Add_color(T, Init(A));
+            }
+            system("cls");
+            Print(T);
+            system("pause");
+            break;
+        }
+        case 2:
+        {
+            while (getline(file, line))
+            {
+                istringstream line_F(line);
+                line_F >> A.filling >> A.weight >> A.restaurant >> A.price;
+                Add_price(T, Init(A));
+            }
+            system("cls");
+            Print(T);
+            system("pause");
+            break;
+        }
+        case 3:
+        {
+            float price;
+            cout << "Price = "; cin >> price;
+            while (getline(file, line))
+            {
+                istringstream line_F(line);
+                line_F >> A.filling >> A.weight >> A.restaurant >> A.price;
+                Add_price(T, Init(A));
+            }
+            system("cls");
+            Print_price(T, price);
+            system("pause");
+            break;
+        }
+        case 4:
+        {
+            char a;
+            cout << "Letter = "; cin >> a;
+            while (getline(file, line))
+            {
+                istringstream line_F(line);
+                line_F >> A.filling >> A.weight >> A.restaurant >> A.price;
+                Add_color(T, Init(A));
+            }
+            system("cls");
+            Print_color(T, a);
+            system("pause");
+            break;
+        }
+        }
+        Delete(T);
+        file.close();
+    }
+    return 0;
+}
