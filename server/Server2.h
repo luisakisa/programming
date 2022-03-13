f6d88307f039af54127c160727e4945505b77509
luisakisa <72152407+luisakisa@users.noreply.github.com>
2022-03-13 15:06:21 +0700

    -constr

--- server/Server2.h
+++ server/Server2.h
@@ -8,8 +8,6 @@ class Server2: public IX, public IY
     private:
     int a, b;
     public:
-    Server2();
-    ~Server2();
     void Func(int c);
     void Func2();
     HRESULT_ QueryInterface(IID_ Iid, void **ppv);
